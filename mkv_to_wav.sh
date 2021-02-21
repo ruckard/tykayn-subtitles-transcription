9f46ed39609a18f2d1a00404e1913d98ff6912ee
tykayn <15d65f2f-0b14-4f70-bf34-e130180ed62b@users.tedomum.net>
2021-02-21 11:07:47 +0100

    formats conversion files, a bash script for each. needs optimisation

--- mkv_to_wav.sh
+++ mkv_to_wav.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# utilisation: bash mp3_to_wav.sh
+# utilisation: bash mkv_to_wav.sh
 # auteur du script: tykayn contact@cipherbliss.com
 
-echo "########### conversion des fichiers audio .mp3 placés dans le dossier input, vers du wav mono-piste uniquement dans le dossier input/converted_to_wav"
+echo "########### conversion des fichiers audio .mkv placés dans le dossier input, vers du wav mono-piste uniquement dans le dossier input/converted_to_wav"
 echo " "
 cd input
 mkdir already_converted
 
-for i in *.mp3; do
+for i in *.mkv; do
 
     echo "fichier à traiter: $i"
-    ffmpeg -i "$i" -ac 1 "converted_to_wav/${i%mp3}wav"
+    ffmpeg -i "$i" -ac 1 "converted_to_wav/${i%mkv}wav"
 
     echo " converti en WAv, déplacement dans le dossier input/already_converted"
     mv "$i" already_converted/
@@ -19,14 +19,14 @@ done
 
 echo " "
 echo "########### OK "
-echo "########### fichiers mp3 restant dans le dossier input "
-ls -l *.mp3
+echo "########### fichiers mkv restant dans le dossier input "
+ls -l *.mkv
 cd ..
 echo " "
-COUNT_LINES_ORIGIN=$(ls -l input/*.mp3 |wc -l)
+COUNT_LINES_ORIGIN=$(ls -l input/*.mkv |wc -l)
 COUNT_LINES=$(ls -l input/converted_to_wav |wc -l)
 
-echo "########### fichiers mp3 dans le dossier input: $COUNT_LINES_ORIGIN "
+echo "########### fichiers mkv dans le dossier input: $COUNT_LINES_ORIGIN "
 echo "########### fichiers wav dans le dossier input/converted_to_wav: $COUNT_LINES "
 echo "########### conversion faite dans output/converted_out_without_nulls.txt"
 exit 0
--- ogg_to_wav.sh
+++ ogg_to_wav.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
-# utilisation: bash transcript.sh MONFICHIER.wav
+# utilisation: bash ogg_to_wav.sh
 # auteur du script: tykayn contact@cipherbliss.com
 
-echo "########### conversion des fichiers audio .ogg dans le dossier input, vers du wav mono-piste uniquement,
- avec Vosk installé par pip3, et un modèle de textes en français."
+echo "########### conversion des fichiers audio .ogg placés dans le dossier input, vers du wav mono-piste uniquement dans le dossier input/converted_to_wav"
 echo " "
-for i in input/*.ogg; do
-  ffmpeg -acodec libvorbis -i "$i" -acodec pcm_s16le "input/converted_to_wav/${i%ogg}wav"
+cd input
+mkdir already_converted
+
+for i in *.ogg; do
+
+    echo "fichier à traiter: $i"
+    ffmpeg -i "$i" -ac 1 "converted_to_wav/${i%ogg}wav"
+
+    echo " converti en WAv, déplacement dans le dossier input/already_converted"
+    mv "$i" already_converted/
 done
+
 
 echo " "
 echo "########### OK "
+echo "########### fichiers ogg restant dans le dossier input "
+ls -l *.ogg
+cd ..
 echo " "
-COUNT_LINES_OGG=$(ll input/*.ogg |wc -l)
-COUNT_LINES=$(ll input/converted_to_wav |wc -l)
+COUNT_LINES_ORIGIN=$(ls -l input/*.ogg |wc -l)
+COUNT_LINES=$(ls -l input/converted_to_wav |wc -l)
 
-echo "########### fichiers ogg dans le dossier input: $COUNT_LINES_OGG "
-echo "########### fichiers wav dans le dossier input: $COUNT_LINES "
+echo "########### fichiers ogg dans le dossier input: $COUNT_LINES_ORIGIN "
+echo "########### fichiers wav dans le dossier input/converted_to_wav: $COUNT_LINES "
 echo "########### conversion faite dans output/converted_out_without_nulls.txt"
 exit 0
