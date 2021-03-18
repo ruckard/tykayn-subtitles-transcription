42fede81e5e514132907be907099a759f43792d8
tykayn <15d65f2f-0b14-4f70-bf34-e130180ed62b@users.tedomum.net>
2021-03-18 11:03:32 +0100

    add index webpage

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+	include('src/download.php');
+	include('src/run.php');
+	?>
+<html>
+<head>
+	<title>
+		Transcription vosk
+	</title>
+</head>
+<body>
+
+<main class='container'>
+	<h1>Transcription</h1>
+	<section>
+		Donnez l'url de la vidéo à transcrire.
+		<form action='' >
+			<input type='text' >
+			<button type='submit'>Envoyer</button>
+		</form >
+	</section>
+	<section class='result'>
+		Voici votre fichier de sous titres:
+
+	</section>
+	<section>
+		<h1>Vie privée</h1>
+		ce site utilise youtube-dl pour récupérer la vidéo à transcrire et travailler dessus sur le serveur où cette page est installée.
+		Vous pouvez aussi installer sur votre propre machine ce logiciel libre sous licence GNU affero v3.
+	</section>
+</main>
+</body>
+</html>
